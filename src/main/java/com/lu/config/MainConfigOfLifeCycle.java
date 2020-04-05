806d890c8e8fb2d502cbee9882c16bb4cd2f84b5
qaz942956627 <942956627@qq.com>
2020-04-05 14:51:09 +0800

    BeanPostProcessor

--- src/main/java/com/lu/config/MainConfigOfLifeCycle.java
+++ src/main/java/com/lu/config/MainConfigOfLifeCycle.java
@@ -14,8 +14,11 @@ import org.springframework.context.annotation.Configuration;
  * 构造(创建对象)
  *      单实例:在容器启动的时候创建对象
  *      多实例:在每次获取的时候创建对象
+ *
+ * BeanPostProcessor.postProcessBeforeInitialization
  * 初始化:
  *      对象创建完成,并赋值好,调用初始化方法...
+ * BeanPostProcessor.postProcessAfterInitialization
  * 销毁:
  *      单实例:容器关闭的时候
  *      多实例:容器不会管理这个bean:容器不会调用销毁方法
@@ -27,6 +30,12 @@ import org.springframework.context.annotation.Configuration;
  * 3.可以使用JSR250:
  *      @PostConstruct: 在bean创建完成并且熟悉复制完成,来执行初始化方法
  *      @PreDestroy: 在容器销毁bean之前通知我们进行清理工作
+ *
+ * 4.BeanPostProcessor[interface] :bean的后置处理器
+ *      postProcessBeforeInitialization: 在初始化之前工作
+ *      postProcessAfterInitialization: 在初始化之后工作
+ *
+ *
  * @author 小卢
  */
 @ComponentScan("com.lu.bean")
